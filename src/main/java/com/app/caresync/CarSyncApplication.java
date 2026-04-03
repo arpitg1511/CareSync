2f08ce1bce0759fe79279eca7321ed55151c44f6
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-04-04 01:05:44 +0530

    [Arpit Gupta] Add . Users schema implemented

---
+++ src/main/java/com/app/caresync/CarSyncApplication.java
@@ -0,0 +1,12 @@
+package com.app.caresync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSyncApplication.class, args);
+    }
+}
---
+++ src/main/java/com/app/caresync/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.app.caresync.model;
+
+public enum UserRole {
+    PATIENT,
+    PROVIDER,
+    ADMIN
+}
