97bebd28cedb845c239a7bb2ee8954b53fea00c5
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-04-04 03:00:04 +0530

    [Arpit Gupta] Add . Added jwtUtils security and userDetails service class

---
+++ src/main/java/com/app/caresync/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.app.caresync.service;
+
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.app.caresync.model.User;
+import com.app.caresync.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		User user = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with email " + email));
+
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPasswordHash(),
+				Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name())));
+	}
+}
