38578843b93d8d198c4cf5aeeaa6a21542b51b50
Arpit Gupta <arpitguptafzd123@gmail.com>
2026-04-04 03:00:37 +0530

    [Arpit Gupta] Add . Added jwtUtils security and userDetails service class

--- src/main/java/com/app/caresync/service/UserDetailsServiceImpl.java
+++ src/main/java/com/app/caresync/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.app.caresync.service;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,7 @@ import com.app.caresync.repository.UserRepository;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	@Autowired
 	private UserRepository userRepository;
 
 	@Override
